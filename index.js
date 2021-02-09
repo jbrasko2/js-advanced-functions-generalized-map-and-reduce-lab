2991a5a7e787f8d557d573114bdde187d59350d9
jbrasko <jbrasko0508@gmail.com>
2021-02-08 18:59:24 -0600

    commit tree 49f6a2c5f200e592f5967c4801e52c5dcabe36cc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(sourceArray, func) {
+    let r = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        let element = sourceArray[i]
+        r.push(func(element))
+    }
+    return r
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
